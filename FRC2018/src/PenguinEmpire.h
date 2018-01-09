f4eaaae7cdd5324447c8b4cefe8066a75ed871ce
Noxilus <noahjgersh@icloud.com>
2018-01-08 19:14:09 -0800

    PenguinEmpire.h

--- FRC2018/src/PenguinEmpire.h
+++ FRC2018/src/PenguinEmpire.h
@@ -9,12 +9,17 @@
 #define SRC_PENGUINEMPIRE_H_
 
 #include "WPILib.h"
+#include "MyJoystick.h"
 
 class Robot : public IterativeRobot {
 public:
 // Components and Systems
 	Joystick left, right, handheld; // Joysticks
+	MyJoystick m_left, m_right, m_handheld;
 	Spark l1, l2, r1, r2; // Drive motor controllers
+
+//Values
+	bool leftauto;
 
 // Stages
 
@@ -26,10 +31,14 @@ public:
 	// Autonomous
 	void AutonomousInit();
 	void AutonomousPeriodic();
+	void CheckSide();
+	void LeftAuto();
+	void RightAuto();
 
 	// Teleoperated
 	void TeleopInit();
 	void TeleopPeriodic();
+	void TankDrive();
 
 	// Test
 	void TestInit();
--- FRC2018/src/Robot.cpp
+++ FRC2018/src/Robot.cpp
@@ -26,11 +26,16 @@ Robot::Robot() : // Robot constructor - Initialize all subsystem and component c
 	r1(pwm2),
 	r2(pwm3)
 {
+	leftauto = false;
+
+	m_left.init(&left);
+	m_right.init(&right);
+	m_handheld.init(&handheld);
 
 }
 
 Robot::~Robot() { // Robot destructor - Delete pointer values here
-
+	delete m_left, m_right, m_handheld;
 }
 
 void Robot::RobotInit() { // Runs only when robot code starts initially
@@ -38,7 +43,7 @@ void Robot::RobotInit() { // Runs only when robot code starts initially
 }
 
 void Robot::AutonomousInit() { // Runs at start of autonomous phase, only once
-
+	CheckSide();
 }
 
 void Robot::AutonomousPeriodic() { // Looped through iteratively during autonomous phase - do not put loops here!
@@ -47,21 +52,70 @@ void Robot::AutonomousPeriodic() { // Looped through iteratively during autonomo
 
 /*
  * Autonomous Functions:
- * None
+ * CheckSide
+ * LeftAuto - wip
+ * RightAuto - wip
  */
+
+void Robot::CheckSide() {
+	std::string gameData;
+	gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
+	if(gameData[0] == 'L')
+	{
+		leftauto = true;
+	} else {
+		leftauto = false;
+	}
+}
+
+void Robot::LeftAuto() {
+
+}
+
+void Robot::RightAuto() {
+
+}
 
 void Robot::TeleopInit() { // Runs at start of teleoperated phase, only once
 
 }
 
 void Robot::TeleopPeriodic() { // Looped through iteratively during teleoperated phase - do not put loops here! Only teleop function calls!
-
+	TankDrive();
 }
 
 /*
  * Teleop Functions:
- * None
+ * TankDrive - temp
  */
+
+void Robot::TankDrive() {
+	double leftInput;
+	double rightInput;
+
+	leftInput = left.GetRawAxis(1);
+	rightInput = right.GetRawAxis(1);
+
+	if(fabs(leftInput) > 0.3) {
+		l1.Set(leftInput * -0.65);
+		l2.Set(leftInput * -0.65);
+	}
+	else
+	{
+		l1.Set(0);
+		l2.Set(0);
+	}
+
+	if(fabs(rightInput) > 0.3) {
+		r1.Set(rightInput * -0.65);
+		r2.Set(rightInput * -0.65);
+	}
+	else
+	{
+		r1.Set(0);
+		r2.Set(0);
+	}
+}
 
 void Robot::TestInit() { // Runs at start of test phase, only once
 
@@ -75,7 +129,6 @@ void Robot::TestPeriodic() { // Looped through iteratively during test phase - d
  * Test Functions:
  * None
  */
-
 
 
 START_ROBOT_CLASS(Robot)
